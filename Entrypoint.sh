c012e98e98a79cfa913302493850c61526a92810
BrandonTsai <emilwings@gmail.com>
2018-05-29 19:58:51 +1000

    initial cloud Formation file

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -7,6 +7,8 @@ prod() {
   export PATH=$PATH:~/.local/bin/
   aws ssm get-parameters --region ap-southeast-2 --names username password database host dialect --with-decryption --query 'Parameters[*].{key:Name,value:Value}' | jq from_entries | jq '{ $node_env : .}' > ./config/database.json
   yarn db-migrate
+  # emil-list should get from S3.
+  cp db/data/email-list-example.csv db/data/email-list.csv
   pm2-runtime server.js
 }
 
